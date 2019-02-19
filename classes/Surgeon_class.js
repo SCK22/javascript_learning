78faf8bb3f88c68f81089a845e5b72e90ab082cf
sck22 <chaithanyakumar.ds@gmail.com>
2019-02-20 00:15:21 +0530

    classes

--- classes/Surgeon_class.js
+++ classes/Surgeon_class.js
@@ -1,24 +1,26 @@
 class Surgeon {
-    constructor(name, department) {
-      this._name = name;
-      this._department = department;
-      this._remainingVacationDays = 20;
-    }
-    get name(){
-      return this._name;
-    }
-    get department(){
-      return this._department;
-    }
-    get remainingVacationDays(){
-     return this._remainingVacationDays;
-    }
-    takeVacationDays(daysoff){
-      this._remainingVacationDays = this._remainingVacationDays-daysoff
-    }
+  constructor(name, department) {
+    this._name = name;
+    this._department = department;
+    this._remainingVacationDays = 20;
   }
-  
-  const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
-  const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
+  get name(){
+    return this._name;
+  }
+  get department(){
+    return this._department;
+  }
+  get remainingVacationDays(){
+   return this._remainingVacationDays;
+  }
+  takeVacationDays(daysoff){
+    this._remainingVacationDays = this._remainingVacationDays-daysoff
+  }
+}
 
-// Methods
+const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
+const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
+
+console.log(surgeonCurry.name);
+surgeonCurry.takeVacationDays(3);
+console.log(surgeonCurry.remainingVacationDays);
